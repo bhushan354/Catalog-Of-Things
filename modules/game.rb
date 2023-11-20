f5f6851966831c713058c18aa85a51b039598d36
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-21 00:54:37 +0100

    created author and item class

--- modules/game.rb
+++ modules/game.rb
@@ -1,19 +1,24 @@
 require 'date'
+require_relative 'Item'
 
-class Game
+class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer = false, last_played_at = nil)
+  def initialize(last_played_at: nil, multiplayer: false, id: nil, publish_date: nil, archived: false)
+    super(id, publish_date, archived)
+
+    raise ArgumentError, 'multiplayer must be a Boolean' unless multiplayer.is_a?(Boolean)
+
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
     return false if @last_played_at.nil?
-    
+
     current_date = Date.today
     last_played_date = Date.parse(@last_played_at)
-    
+
     super && (last_played_date < current_date - (365 * 2))
   end
 end
---
+++ modules/item.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+class Item
+  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :id, :archived
+
+  def initialize(id = Random.rand(1..1000), publish_date = DateTime.now, archived: false)
+    @id = id
+    @archived = archived
+    @publish_date = publish_date
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+  end
+
+  def can_be_archived?
+    publish_date && publish_date < (Date.today - (365 * 10))
+  end
+
+  def move_to_archive
+    @archived = can_be_archived?
+  end
+end
