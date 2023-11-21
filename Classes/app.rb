fbf17ec358e88addfa90acbd3002856b43d7663c
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-21 09:12:29 +0530

    Rubocop errors fix

--- Classes/app.rb
+++ Classes/app.rb
@@ -1,16 +1,16 @@
 class App
-    def initialize
-      @all_things = []
-    end
-  
-    def create_book
-      print 'Date of publishing: '
-      date = gets.chomp
-  
-      book = Book.new(date)
-      @all_things << book
-      puts 'Item Created Successfully'
-    end
-
-    #you can add your required def here
+  def initialize
+    @all_things = []
   end
+
+  def create_book
+    print 'Date of publishing: '
+    date = gets.chomp
+
+    book = Book.new(date)
+    @all_things << book
+    puts 'Item Created Successfully'
+  end
+
+  # you can add your required def here
+end
--- Classes/item.rb
+++ Classes/item.rb
@@ -1,37 +1,36 @@
 class Item
-    attr_accessor :id, :author, :source, :label, :publish_date, :archived
-  
-    def initialize(publish_date)
-      @id = generate_id
-      @author = nil
-      @source = nil
-      @label = nil
-      @publish_date = publish_date
-      @archived = false
-    end
-  
-    def can_be_archived?
-      years_difference > 10
-    end
-  
-    def move_to_archive
-      result = can_be_archived?
-      @archived = true if result
-      nil
-    end
-  
-    private
-  
-    def generate_id
-      Random.rand(1..1000)
-    end
-  
-    def years_difference
-      current_year - @publish_date.year
-    end
-  
-    def current_year
-      Time.new.year
-    end
+  attr_accessor :id, :author, :source, :label, :publish_date, :archived
+
+  def initialize(publish_date)
+    @id = generate_id
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = false
   end
-  
+
+  def can_be_archived?
+    years_difference > 10
+  end
+
+  def move_to_archive
+    result = can_be_archived?
+    @archived = true if result
+    nil
+  end
+
+  private
+
+  def generate_id
+    Random.rand(1..1000)
+  end
+
+  def years_difference
+    current_year - @publish_date.year
+  end
+
+  def current_year
+    Time.new.year
+  end
+end
