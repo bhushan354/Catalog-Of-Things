ecd976c2adabbf3928d595e6b940c910383778f0
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-21 08:54:35 +0530

    Add Create_book defination in app.rb

---
+++ Classes/app.rb
@@ -0,0 +1,14 @@
+class App
+    def initialize
+      @all_things = []
+    end
+  
+    def create_book
+      print 'Date of publishing: '
+      date = gets.chomp
+  
+      book = Book.new(date)
+      @all_things << book
+      puts 'Item Created Successfully'
+    end
+  end
