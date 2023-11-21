933d8cd07be34f9a0499ffc48ffd0cfd75b3b298
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-21 10:42:15 +0100

    made changes

--- Classes/genre.rb
+++ Classes/genre.rb
@@ -1,23 +1,21 @@
-# genre.rb
 require_relative 'item'
 
 class Genre
-  attr_accessor :id, :name, :items
+  attr_accessor :id, :name
+  attr_reader :items
 
-  def initialize(name)
-    @id = generate_id
+  def initialize(name, id = Random.rand(1..1000))
+    raise ArgumentError, 'name must be a non-empty string' unless name.is_a?(String) && !name.empty?
+
+    @id = id
     @name = name
     @items = []
   end
 
   def add_item(item)
+    raise ArgumentError, 'item must be an instance of Item or its subclass' unless item.is_a?(Item)
+
     @items << item
     item.genre = self
-  end
-
-  private
-
-  def generate_id
-    Random.rand(1..1000)
   end
 end
