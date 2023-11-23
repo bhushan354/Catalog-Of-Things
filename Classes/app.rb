d5599b864430a8542fe70eee48313cd4869d6557
Ochiengsteven <stevenochieng6305@gmail.com>
2023-11-23 10:49:10 +0300

    ADD METHODS TO APP RB

--- Classes/app.rb
+++ Classes/app.rb
@@ -2,6 +2,7 @@ require 'date'
 require_relative 'game'
 require_relative 'author_app'
 require_relative 'utils/util'
+require_relative 'music_album'
 
 class App
   def initialize
@@ -9,7 +10,7 @@ class App
 
     # All subclass of item class (Book, MusicAlbum, Movie, and Game) should be push here
     @items = []
-
+    @music_albums = []
     @author_manager = AuthorApp.new
   end
 
@@ -90,4 +91,50 @@ class App
     end
   end
   # you can add your required def here
+
+  def create_music_album
+    puts 'Creating a new music album:'
+
+    print 'Enter label: '
+    label = gets.chomp
+
+    print 'Enter author: '
+    author = gets.chomp
+
+    print 'Is the album on Spotify? (true/false): '
+    on_spotify = gets.chomp.downcase == 'true'
+
+    print 'Enter the publish date (YYYY-MM-DD, press Enter for today): '
+    publish_date_input = gets.chomp
+    publish_date = parse_publish_date(publish_date_input)
+
+    puts "The publish date is #{publish_date}"
+
+    music_album = MusicAlbum.new(label: label, author: author, on_spotify: on_spotify, publish_date: publish_date)
+    @music_albums << music_album
+
+    puts 'Music album created successfully!'
+  end
+
+  def list_all_music_albums
+    puts 'List of all music albums:'
+
+    @music_albums.each do |album|
+      puts "[#{album.publish_date}] Label: #{album.label}, Author: #{album.author}"
+    end
+    puts
+  end
+
+  private
+
+  def parse_publish_date(input)
+    return Date.today if input.empty?
+
+    begin
+      Date.parse(input)
+    rescue ArgumentError
+      puts 'Invalid date format. Using today\'s date.'
+      Date.today
+    end
+  end
 end
