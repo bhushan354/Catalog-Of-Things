cae6c1178f86c53323ba8cc4a5148153f97e9c8c
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-25 08:24:00 +0100

    unit test

--- Classes/spec/genre_spec.rb
+++ Classes/spec/genre_spec.rb
@@ -1,57 +1,36 @@
-genre_spec.rb
-
-require_relative '../Classes/item'
-require_relative '../Classes/genre'
+require_relative '../genre'
+require_relative '../music_album'
+require 'rspec'
 
 describe Genre do
+  let(:genre_name) { 'Pop' }
+  let(:genre_id) { 123 }
+
+  let(:music_album) { MusicAlbum.new }
+  let(:non_item) { double('NonItem', is_a?: false) }
+
+  subject(:genre) { Genre.new(genre_name, genre_id) }
+
   describe '#initialize' do
-    context 'with valid parameters' do
-      it 'creates a Genre instance' do
-        genre = Genre.new('Rock')
-        expect(genre).to be_a(Genre)
-      end
+    it 'initializes a genre with a name and id' do
+      expect(genre.name).to eq(genre_name)
+      expect(genre.id).to eq(genre_id)
+    end
 
-      it 'sets the name attribute' do
-        genre = Genre.new('Rock')
-        expect(genre.name).to eq('Rock')
-      end
-
-      it 'sets a random id if not provided' do
-        allow(Random).to receive(:rand).and_return(42)
-        genre = Genre.new('Pop')
-        expect(genre.id).to eq(42)
-      end
-
-      it 'raises an error if name is not a non-empty string' do
-        expect { Genre.new(nil) }.to raise_error(ArgumentError)
-        expect { Genre.new('') }.to raise_error(ArgumentError)
-        expect { Genre.new(123) }.to raise_error(ArgumentError)
-      end
+    it 'raises an error if the name is not a non-empty string' do
+      expect { Genre.new(nil) }.to raise_error(ArgumentError)
+      expect { Genre.new('') }.to raise_error(ArgumentError)
     end
   end
 
   describe '#add_item' do
-    context 'with a valid item' do
-      it 'adds the item to the genre' do
-        genre = Genre.new('Jazz')
-        item = Item.new(1, Date.today)
-        genre.add_item(item)
-        expect(genre.items).to include(item)
-      end
-
-      it 'sets the genre of the item' do
-        genre = Genre.new('Jazz')
-        item = Item.new(1, Date.today)
-        genre.add_item(item)
-        expect(item.genre).to eq(genre)
-      end
+    it 'adds an item to the genre' do
+      genre.add_item(music_album)
+      expect(genre.items.size).to eq(1)
     end
 
-    context 'with an invalid item' do
-      it 'raises an error if item is not an instance of Item or its subclass' do
-        genre = Genre.new('Hip Hop')
-        expect { genre.add_item('invalid') }.to raise_error(ArgumentError)
-      end
+    it 'raises an error if the item is not an instance of Item or its subclass' do
+      expect { genre.add_item(non_item) }.to raise_error(ArgumentError)
     end
   end
 end
